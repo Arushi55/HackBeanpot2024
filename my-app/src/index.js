98dd1c8c1c37db232e024f1d9f0afb023087eb53
yomi-addey <addey.a@northeastern.edu>
2024-02-24 11:54:11 -0500

    format changes

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -3,11 +3,8 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App.js';
 import reportWebVitals from './reportWebVitals.js';
-<<<<<<< HEAD
 
 <script type="module" src="linear-algebra.js"></script>
-=======
->>>>>>> 32ae311b9e4477c5c3625539318a2b190125ada2
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
