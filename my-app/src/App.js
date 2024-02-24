cb53c676adee3d9a6e0b061ac12b77406420cfdd
Arushi Aggarwal <arushi3.14@outlook.com>
2024-02-24 07:49:27 -0500

    created one cake object, trying to render now

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,9 +1,10 @@
+import Recipe from recipe.js;
 
 export default function MyApp() {
   return (
     <div>
       <h1>Welcome to my app</h1>
-      
+      <Recipe />
     </div>
   );
 }
--- my-app/src/recipe.js
+++ my-app/src/recipe.js
@@ -11,22 +11,25 @@ const cake_recipe = {
     2. Preheat the oven to 350 degrees F (175 degrees C). Grease and flour a 9-inch square cake pan. \n \
     3. Cream sugar and butter together in a mixing bowl. Add eggs, one at a time, beating briefly after each addition. Mix in vanilla. \n \
     4. Combine flour and baking powder in a separate bowl. Add to the wet ingredients and mix well. Add milk and stir until smooth. \n \
-    "
+    5. Pour batter into the prepared cake pan. \n \
+    6. Bake in the preheated oven until the top springs back when lightly touched, 30 to 40 minutes.\n \
+    7. Remove from the oven and cool completely."
   };
 
-  export default function Profile() {
+  export default function Recipe() {
     return (
       <>
-        <h1>{user.name}</h1>
-        <img
-          className="avatar"
-          src={user.imageUrl}
-          alt={'Photo of ' + user.name}
-          style={{
-            width: user.imageSize,
-            height: user.imageSize
-          }}
-        />
+        <h1>{cake_recipe.name}</h1>
+        <div className="recipe"/>
+            <li>{cake_recipe.flour}</li>
+            <li>{cake_recipe.sugar}</li>
+            <li>{cake_recipe.butter}</li>
+            <li>{cake_recipe.egg}</li>
+            <li>{cake_recipe.vanilla}</li>
+            <li>{cake_recipe.baking_powder}</li>
+            <li>{cake_recipe.milk}</li>
+            <li>{cake_recipe.steps}</li>
+        <div/>
       </>
     );
   }
