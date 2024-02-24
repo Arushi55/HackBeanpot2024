b2b0aa9f12f5a75d562d9753ea2986b251f72c66
Arushi Aggarwal <arushi3.14@outlook.com>
2024-02-24 08:47:36 -0500

    added steps to cupcake

--- my-app/src/recipe.js
+++ my-app/src/recipe.js
@@ -29,7 +29,13 @@ const cake_recipe = {
     salt: 1,
     oil: .5,
     water: 1,
-    steps: ""
+    steps: "1. Preheat oven to 350°F (176°C) and prepare a cupcake pan with liners. \
+    2.  Add the flour, sugar, baking powder and salt to a large mixer bowl and combine. Set aside. \
+    3. Add the milk, vegetable oil, vanilla extract and eggs to a medium sized bowl and combine. \
+    4. Add the wet ingredients to the dry ingredients and beat until well combined. \
+    5. Slowly add the water to the batter and mix on low speed until well combined. Scrape down the sides of the bowl as needed to make sure everything is well combined. Please note that the batter will be very thin.\
+    6. Fill the cupcake liners about half way and bake for 15-17 minutes, or until a toothpick comes out with a few moist crumbs. \
+    7. Remove the cupcakes from oven and allow to cool for 2 minutes, then remove to a cooling rack to finish cooling."
   }
 
   const all_recipes = [cake_recipe, cupcake_recipe];
@@ -39,14 +45,14 @@ const cake_recipe = {
       all_recipes.map(recipe => <>
         <h1>{cake_recipe.name}</h1>
         <div className="recipe"/>
-            <li>Flour: {cake_recipe.flour}</li>
-            <li>Sugar: {cake_recipe.sugar}</li>
-            <li>Butter: {cake_recipe.butter}</li>
-            <li>Eggs: {cake_recipe.egg}</li>
-            <li>Vanilla: {cake_recipe.vanilla}</li>
-            <li>Baking Powder: {cake_recipe.baking_powder}</li>
-            <li>Milk: {cake_recipe.milk}</li>
-            <li>Steps: {cake_recipe.steps}</li>
+            <li>Flour: {recipe.flour}</li>
+            <li>Sugar: {recipe.sugar}</li>
+            <li>Butter: {recipe.butter}</li>
+            <li>Eggs: {recipe.egg}</li>
+            <li>Vanilla: {recipe.vanilla}</li>
+            <li>Baking Powder: {recipe.baking_powder}</li>
+            <li>Milk: {recipe.milk}</li>
+            <li>Steps: {recipe.steps}</li>
         <div/>
       </>)
     );
