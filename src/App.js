f9a585089b97c68380380e88f20883976875b61c
yomi-addey <addey.a@northeastern.edu>
2024-02-24 23:37:49 -0500

    added a header; additional style changes to the form/recipes

--- src/App.js
+++ src/App.js
@@ -2,18 +2,22 @@ import React, { useState } from 'react';
 import './App.css';
 import Recipe from './recipe.js';
 import Search from './search.js';
-import { AppContext} from "./AppContext.js";
+import { AppContext } from "./AppContext.js";
 
 function App() {
   const [appState, setAppState] = useState({
-    inputs: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0]
+    inputs: []
   });
   return (
-    <AppContext.Provider value={{appState, setAppState}}> 
-    <div>
-      <Search />
-      <Recipe />
-    </div>
+    <AppContext.Provider value={{ appState, setAppState }}>
+      <div>
+        <div className='header'>
+          <h1 id="title">The Perfect Pantry</h1>
+          <h4 id="instructions">Tell us the amount of ingredients in your pantry, and we'll give you the recipe that most closely matches those ingredients!</h4>
+        </div>
+        <Search />
+        <Recipe />
+      </div>
     </AppContext.Provider>
   );
 }
